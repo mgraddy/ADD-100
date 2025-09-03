d5a607e2e11fc283cfce8b61693ab5f4cab77e32
Merrick Graddy <mgraddy@students.mchenry.edu>
2025-09-02 22:01:46 -0500

    Add files via upload

---
+++ PE1_2-2.py
@@ -0,0 +1,21 @@
+"""Copy the sample program into your Python editor. Run the program and observe the output. Change the variable values, test the program, and submit your file. """
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 29)
+
+# Integer
+y = -87
+print("Integer:", y)
+
+# Float
+z = 402.15
+print("Float:", z)
+
+# String
+name = "Merrick"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
