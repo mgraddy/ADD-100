349c81d85cb1cee44068e907b06ebb5ccefd6a4b
Merrick Graddy <mgraddy@students.mchenry.edu>
2025-09-08 13:32:34 -0500

    dwawaf

---
+++ PE1_2-5.py
@@ -0,0 +1,15 @@
+'''Type the above code into Visual Studio Code.
+Add a minimum of 4 comments to the above Python program to make it more understandable.'''
+# This program calculates a car's fuel efficiency
+
+# Variable for fuel tank capacity in gallons
+tank_size = 15 
+
+# Define the total distance covered
+# Total distance covered in miles
+distance = 600 
+# Calculate miles per gallon
+mpg = distance / tank_size
+# Print the result
+print("Miles per gallon:", mpg) # prints results
+# This is a lot of comments I don't know what else to write
