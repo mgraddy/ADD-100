a33a70caf9041e969ed5b3d10d9ee06ce589414b
Merrick Graddy <mgraddy@students.mchenry.edu>
2025-09-09 16:26:07 -0500

    3.2B Changed

--- PE1_3-2B.py
+++ PE1_3-2B.py
@@ -1,4 +1,4 @@
 '''Write a Python program to find and print all numbers divisible by 7 between 1 and 300. Use a for loop and the modulus operator (%).'''
-for i in range (1, 300):
-    if i % 7 == 0:
-        print(i)
+for i in range (1, 300): # Sets bounds for search
+    if i % 7 == 0: # Determines if each number is divisible by 7
+        print(i) # Prints number
